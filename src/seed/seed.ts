ce412e4c61df755cef80e857131e8d77af349cd5
Rakshit Sain <rakshitsain895@gmail.com>
2024-08-19 13:11:58 -0400

    seed file

---
+++ src/seed/seed.ts
@@ -0,0 +1,70 @@
+
+
+import { userStatus } from "../utils/types";
+import myDataSource from "../app-data-source";
+import userService from "../services/user.service";import users from "./user.json";
+
+const roles = ["admin", "employee"];
+const subRoles = ["Developer", "Designer", "Tester", "Manager", "Team Leader"];
+
+// insert into database
+export async function insertRolesAndSubRoles() {
+  // Insert roles
+  for (const roleName of roles) {
+    let newRole = Role.create({
+      name: roleName.toLowerCase(),
+    });
+    await roleService.insertRoleIfNotExists(newRole);
+  }
+
+  // insert admin sub roles
+  const adminRole = await roleService.insertRoleIfNotExists(
+    Role.create({ name: "admin" })
+  );
+  let newSubRole = SubRole.create({
+    name: "admin",
+    role: adminRole,
+  });
+  await subRoleService.insertSubRoleIfNotExists(newSubRole);
+
+  // Insert subroles
+  for (const subRoleName of subRoles) {
+    const role = await roleService.insertRoleIfNotExists(
+      Role.create({ name: "employee" })
+    );
+    let newSubRole = SubRole.create({
+      name: subRoleName.toLowerCase(),
+      role: role,
+    });
+    await subRoleService.insertSubRoleIfNotExists(newSubRole);
+  }
+
+  // Insert users
+  for (const user of users) {
+    const role = await roleService.insertRoleIfNotExists(
+      Role.create({
+        name: user.role.toLowerCase(),
+      })
+    );
+    const subRole = await subRoleService.insertSubRoleIfNotExists(
+      SubRole.create({
+        name: user.subrole.toLowerCase(),
+        role: role,
+      })
+    );
+    let newUser = User.create({
+      firstName: user.firstname,
+      lastName: user.lastname,
+      email: user.email,
+      password: user.password,
+      status: user.status as userStatus,
+      role,
+      subRole,
+    });
+    await userService.create(newUser);
+  }
+}
+myDataSource
+  .initialize()
+  .then(() => insertRolesAndSubRoles())
+  .catch(console.error);
